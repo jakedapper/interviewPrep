7e024481a9f57f426d6acef76a6a3d386a6d4cea
jake dapper <jakedapper@gmail.com>
2023-05-07 11:12:42 -0500

    more bubble sorts

--- exercises/sorting/index.js
+++ exercises/sorting/index.js
@@ -14,6 +14,57 @@ function bubbleSort(arr) {
     return arr
 }
 
+//bubbleSort v2
+// UNOPTIMIZED VERSION OF BUBBLE SORT
+// function bubbleSort(arr){
+//     for(var i = arr.length; i > 0; i--){
+//       for(var j = 0; j < i - 1; j++){
+//         console.log(arr, arr[j], arr[j+1]);
+//         if(arr[j] > arr[j+1]){
+//           var temp = arr[j];
+//           arr[j] = arr[j+1];
+//           arr[j+1] = temp;         
+//         }
+//       }
+//     }
+//     return arr;
+//   }
+  
+//   // ES2015 Version
+//   function bubbleSort(arr) {
+//     const swap = (arr, idx1, idx2) => {
+//       [arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]];
+//     };
+  
+//     for (let i = arr.length; i > 0; i--) {
+//       for (let j = 0; j < i - 1; j++) {
+//         if (arr[j] > arr[j + 1]) {
+//           swap(arr, j, j + 1);
+//         }
+//       }
+//     }
+//     return arr;
+//   }
+  
+
+// Optimized BubbleSort with noSwaps
+// function bubbleSort(arr){
+//     var noSwaps;
+//     for(var i = arr.length; i > 0; i--){
+//       noSwaps = true;
+//       for(var j = 0; j < i - 1; j++){
+//         if(arr[j] > arr[j+1]){
+//           var temp = arr[j];
+//           arr[j] = arr[j+1];
+//           arr[j+1] = temp;
+//           noSwaps = false;         
+//         }
+//       }
+//       if(noSwaps) break;
+//     }
+//     return arr;
+//   }
+
 function selectionSort(arr) {
     for (let i = 0; i < arr.length; i++) {
         let indexOfMin = i
