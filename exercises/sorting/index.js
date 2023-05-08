987548067c5b1f041978f3afc558ab82381c67e6
jake dapper <jakedapper@gmail.com>
2023-05-07 20:39:12 -0500

    quicksort

--- exercises/sorting/index.js
+++ exercises/sorting/index.js
@@ -86,7 +86,7 @@ function selectionSort(arr) {
 }
 
 function mergeSort(arr) {
-    if (arr.length ===1){
+    if (arr.length === 1){
         return arr;
     }
 
@@ -110,5 +110,70 @@ function merge(left, right) {
 
     return [...results, ...left, ...right]
 }
+
+
+//from other algorithm course
+
+//MERGE SORT - O(n + m)
+// function merge(arr1, arr2){
+//     let results = [];
+//     let i = 0;
+//     let j = 0;
+//     while(i < arr1.length && j < arr2.length){
+//         if(arr2[j] > arr1[i]){
+//             results.push(arr1[i]);
+//             i++;
+//         } else {
+//             results.push(arr2[j])
+//             j++;
+//         }
+//     }
+//     while(i < arr1.length) {
+//         results.push(arr1[i])
+//         i++;
+//     }
+//     while(j < arr2.length) {
+//         results.push(arr2[j])
+//         j++;
+//     }
+//     return results;
+// }
+
+// function mergeSort(arr){
+//     if(arr.length <= 1) return arr;
+//     let mid = Math.floor(arr.length/2);
+//     let left = mergeSort(arr.slice(0,mid));
+//     let right = mergeSort(arr.slice(mid));
+//     return merge(left, sright);
+// }
+
+//Quick Sort
+function swap(array, firstIndex, secondIndex){
+    let temp = array[firstIndex]
+    array[firstIndex] = array[secondIndex]
+    array[secondIndex] = temp
+}
+
+function pivot(array, pivotIndex  = 0, endIndex = array.length-1){
+    let swapIndex = pivotIndex
+    for(let i = pivotIndex + 1; i <= endIndex; i++){
+        if(array[i] < array[pivotIndex]){
+            swapIndex ++
+            swap(array, swapIndex, i)
+        }
+    }
+    swap(array, pivotIndex, swapIndex)
+    return swapIndex
+}
+
+function quickSort(array, left = 0, right = array.length-1){
+    let pivotIndex = pivot(array, left, right)
+    if(left < right) {
+        pivot(array.slice(left, pivotIndex - 1))
+        pivot(array.slice(pivotIndex +1, right))
+    }
+    return array
+}
+
 
 module.exports = { bubbleSort, selectionSort, mergeSort, merge };
